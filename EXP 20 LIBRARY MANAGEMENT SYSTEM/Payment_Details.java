11905cb3983427dff79663d31c3e4bb3a3a7b875
Selmeya I <112368898+Selmeya@users.noreply.github.com>
2023-08-21 13:50:20 +0530

    Add files via upload

---
+++ EXP 20 LIBRARY MANAGEMENT SYSTEM/Payment_Details.java
@@ -0,0 +1,55 @@
+
+
+/**
+ * Class Payment_Details
+ */
+public class Payment_Details {
+
+  //
+  // Fields
+  //
+
+  private void payment_for_the_book;
+  
+  //
+  // Constructors
+  //
+  public Payment_Details () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of payment_for_the_book
+   * @param newVar the new value of payment_for_the_book
+   */
+  private void setPayment_for_the_book (void newVar) {
+    payment_for_the_book = newVar;
+  }
+
+  /**
+   * Get the value of payment_for_the_book
+   * @return the value of payment_for_the_book
+   */
+  private void getPayment_for_the_book () {
+    return payment_for_the_book;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void paying_method()
+  {
+  }
+
+
+}
---
+++ EXP 24 ONLINE STOCK MARKET/Store_Class.java
@@ -0,0 +1,79 @@
+
+
+/**
+ * Class Store_Class
+ */
+public class Store_Class {
+
+  //
+  // Fields
+  //
+
+  private void store_id;
+  private void store_name;
+  
+  //
+  // Constructors
+  //
+  public Store_Class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_id
+   * @param newVar the new value of store_id
+   */
+  private void setStore_id (void newVar) {
+    store_id = newVar;
+  }
+
+  /**
+   * Get the value of store_id
+   * @return the value of store_id
+   */
+  private void getStore_id () {
+    return store_id;
+  }
+
+  /**
+   * Set the value of store_name
+   * @param newVar the new value of store_name
+   */
+  private void setStore_name (void newVar) {
+    store_name = newVar;
+  }
+
+  /**
+   * Get the value of store_name
+   * @return the value of store_name
+   */
+  private void getStore_name () {
+    return store_name;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void addstore()
+  {
+  }
+
+
+  /**
+   */
+  public void searchstore()
+  {
+  }
+
+
+}
